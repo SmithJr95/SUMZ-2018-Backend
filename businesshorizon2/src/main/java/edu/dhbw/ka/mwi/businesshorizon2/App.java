20ded0c2aeaa1cc93946ed399e6350e096ffbf36
Fischer <Jan.Fischer@realtech.com>
2018-06-04 19:04:11 +0200

    Spring Base Project

---
+++ businesshorizon2/src/main/java/edu/dhbw/ka/mwi/businesshorizon2/App.java
@@ -0,0 +1,18 @@
+package edu.dhbw.ka.mwi.businesshorizon2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+        System.out.println( "App is running." );
+    }
+}
