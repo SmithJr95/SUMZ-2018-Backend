688719144700c13d6799e22069691287d7b3256c
Fischer <Jan.Fischer@realtech.com>
2018-06-09 12:12:45 +0200

    added basic authentication config - not building

---
+++ businesshorizon2/src/main/java/edu/dhbw/ka/mwi/businesshorizon2/dataaccess/repositories/UserRepository.java
@@ -0,0 +1,5 @@
+package edu.dhbw.ka.mwi.businesshorizon2.dataaccess.repositories;
+
+public class UserRepository {
+
+}
---
+++ businesshorizon2/src/main/java/edu/dhbw/ka/mwi/businesshorizon2/models/mappers/UserMapper.java
@@ -0,0 +1,14 @@
+package edu.dhbw.ka.mwi.businesshorizon2.models.mappers;
+
+import edu.dhbw.ka.mwi.businesshorizon2.models.daos.UserDao;
+import edu.dhbw.ka.mwi.businesshorizon2.models.dtos.UserDto;
+
+public class UserMapper {
+	public static UserDao mapToDao(UserDto userDto) {
+		throw new UnsupportedOperationException();
+	}
+	
+	public static UserDto mapToDto(UserDao userDao) {
+		throw new UnsupportedOperationException();
+	}
+}
