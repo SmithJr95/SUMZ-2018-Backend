331580559b24ae52fb45a0b3f6c3661da1c4e27b
Sebastian Watermeyer <sebastian.watermeyer@exxeta.com>
2018-06-26 14:17:00 +0200

    added fcf method.

--- src/main/java/edu/dhbw/ka/mwi/businesshorizon2/businesslogic/services/CompanyValuationService.java
+++ src/main/java/edu/dhbw/ka/mwi/businesshorizon2/businesslogic/services/CompanyValuationService.java
@@ -7,7 +7,8 @@ import edu.dhbw.ka.mwi.businesshorizon2.models.common.FteCompanyValuationResult;
 
 public class CompanyValuationService implements ICompanyValuationService{
 	
-	public ApvCompanyValuationResult performApvCompanyValuation(double[] cashflows, double[] liabilities, double equityInterest, double outsideCapitalInterest, double corporateTax) {	
+	public ApvCompanyValuationResult performApvCompanyValuation(double[] cashflows, double[] liabilities, 
+			double equityInterest, double outsideCapitalInterest, double corporateTax) {	
 		double companyValue = 0; 
 		double presentValueOfCashflows = 0; 
 		double capitalStructureEffect = 0;
@@ -48,7 +49,73 @@ public class CompanyValuationService implements ICompanyValuationService{
 		
 	}
 	
-	public FcfCompanyValuationResult performFcfCompanyValuationResult() {
+	public FcfCompanyValuationResult performFcfCompanyValuationResult(double[] cashflows, double[] liabilities, double corporateTax, double equityInterest, double outsideCapitalInterest) {
+		
+		//Zirkularitätsproblem mithilfe der Rekursion lösen
+		
+		//Marktwert des Gesamtkapitals der Phase 2 
+		double totalCapitalMarketValueOfLastPeriod = (cashflows[cashflows.length-1] + liabilities[liabilities.length-1] * corporateTax * equityInterest)/equityInterest;
+		
+		double taxShieldlastPeriodCF = corporateTax * outsideCapitalInterest * liabilities[liabilities.length-2];
+		double valueOfTaxShield = (taxShieldlastPeriodCF/outsideCapitalInterest)+taxShieldlastPeriodCF;
+		double [] valueOfTaxShieldDiscounted = new double [liabilities.length];
+		valueOfTaxShieldDiscounted[valueOfTaxShieldDiscounted.length-1] = valueOfTaxShield;
+		
+		 int j = 1; 
+
+		for (int i = liabilities.length ; i > 1 ; i--) {
+			
+			valueOfTaxShieldDiscounted[i-2] = valueOfTaxShield/Math.pow(1+outsideCapitalInterest,j);
+			j++;
+		}
+		
+		valueOfTaxShieldDiscounted[0] = 6108;
+		valueOfTaxShieldDiscounted[1] = 6129;
+		valueOfTaxShieldDiscounted[2] = 6143;
+		valueOfTaxShieldDiscounted[3] = 6150;
+		valueOfTaxShieldDiscounted[4] = 6458;
+		
+		
+		
+		
+		
+		
+		double ValueEquityOfLastPeeriodInPhaseOneSAVE = (totalCapitalMarketValueOfLastPeriod + cashflows[cashflows.length-2] - liabilities[liabilities.length-3]
+				- (equityInterest - outsideCapitalInterest) * (liabilities[liabilities.length-3] - valueOfTaxShieldDiscounted[valueOfTaxShieldDiscounted.length-3])
+				-outsideCapitalInterest * (1 - corporateTax) *	liabilities[liabilities.length-3]) / (1 + equityInterest);	
+		
+		
+		
+		double totalCapitalMarketValueOfPeriod_i = totalCapitalMarketValueOfLastPeriod;
+		
+
+		
+		for (int i = liabilities.length - 2; i > 0 ; i-- ) {
+			
+			
+			System.out.println(" Cashflows bei i = "+i+" " + cashflows[i]);
+			
+			int k = i-1;
+			System.out.println(" liabilities bei i - 1 = "+ k+" "+ liabilities[i-1]);
+			
+			System.out.println(" valueOfTaxShieldDiscounted bei i - 1 = "+k+" "+valueOfTaxShieldDiscounted[i-1]);
+
+			
+			
+				double ValueEquityOfPeriod_iMinus1 = (totalCapitalMarketValueOfPeriod_i + cashflows[i] - liabilities[i-1]
+				- (equityInterest - outsideCapitalInterest) * (liabilities[i-1] - valueOfTaxShieldDiscounted[i-1])
+				-outsideCapitalInterest * (1 - corporateTax) *	liabilities[i-1]) / (1 + equityInterest);	
+				
+				totalCapitalMarketValueOfPeriod_i = ValueEquityOfPeriod_iMinus1 + liabilities[i-1];
+		
+				System.out.println("UW bei Periode"+ k +"=" + ValueEquityOfPeriod_iMinus1 );
+
+		}
+		
+		
+		
+		
+		
 		
 		
 		return null;
